529abfbbfa525ae83e09456eebf377b3de58a466
Dave <davidlongswe@gmail.com>
2022-12-20 22:24:22 +0100

    Playable

--- src/components/Cell.jsx
+++ src/components/Cell.jsx
@@ -1,10 +1,9 @@
 import React from "react";
-
 import { StyledCell } from "./styles/StyledCell";
 import { TETROMINOS } from "../tetrominos";
 
-const Cell = ({ type }) => {
-  return <StyledCell type={type} color={TETROMINOS[type].color}></StyledCell>;
-};
+const Cell = ({ type }) => (
+  <StyledCell type={type} color={TETROMINOS[type].color}></StyledCell>
+);
 
-export default Cell;
+export default React.memo(Cell);
